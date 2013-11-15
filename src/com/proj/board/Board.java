ecc8cadbcf05ff168c6c1e6a0e768c1e025c64c3
RyanTM899213 <rtm773@mun.ca>
2013-11-15 11:28:42 -0330

    Beefing Board class

--- src/com/proj/board/Board.java
+++ src/com/proj/board/Board.java
@@ -3,10 +3,22 @@ package com.proj.board;
 /** This class controls the 2D Board in the game, as well as all necessary implementation */
 
 public class Board {
+	private int boardSize=15;
+	private Tile[][] tiles;  // the 2D array of tiles
 	
 	public Board() {
-		Tile[][] tiles=new Tile[15][15];  // the 2D array of tiles
+		tiles=new Tile[boardSize][boardSize];
 	}
+	
+	public boolean isOccupied(int width, int height) {
+		if ((width>boardSize) || (height>boardSize)) {
+			throw new IndexOutOfBoundsException(">> Invalid array bounds");
+		}
+		return tiles[width][height]==null;
+	}
+	
+	
+	
 	
 
 }
--- src/com/proj/board/Tile.java
+++ src/com/proj/board/Tile.java
@@ -3,10 +3,11 @@ package com.proj.board;
 /** This class imitates each tile that is used on the board for the game */
 
 public class Tile {
-
+	
 	public Tile() {
 		
 	}
+	
 	
 	
 }
