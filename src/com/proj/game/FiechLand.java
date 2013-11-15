2de9e90a5eae1221b71bca2cd734d442562d0472
tylerstacey <tyler.stacey@outlook.com>
2013-11-15 09:50:26 -0330

    Get the main class set-up

--- src/com/proj/game/FiechLand.java
+++ src/com/proj/game/FiechLand.java
@@ -1,7 +1,61 @@
 package com.proj.game;
 
-public class FiechLand {
-	public static void main (String[] args){
-		//TODO Start the game from here.
+import java.awt.Container;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JFrame;
+
+public class FiechLand extends JFrame implements WindowListener{
+	private static int DEFAULT_FPS = 40;
+
+	private FiechLandPanel flp;
+
+	public FiechLand(long period) {
+		super("FiechLand");
+
+		Container c = getContentPane(); // default BorderLayout used
+		flp = new FiechLandPanel(this, period);
+		c.add(flp, "Center");
+
+		addWindowListener(this);
+		pack();
+		setResizable(false);
+		setVisible(true);
+	} //End of FiechLand constructor
+	
+	// ----------------- window listener methods -------------
+
+		public void windowActivated(WindowEvent e) {
+			flp.resumeGame();
+		}
+
+		public void windowDeactivated(WindowEvent e) {
+			flp.pauseGame();
+		}
+
+		public void windowDeiconified(WindowEvent e) {
+			flp.resumeGame();
+		}
+
+		public void windowIconified(WindowEvent e) {
+			flp.pauseGame();
+		}
+
+		public void windowClosing(WindowEvent e) {
+			flp.stopGame();
+		}
+
+		public void windowClosed(WindowEvent e) {
+		}
+
+		public void windowOpened(WindowEvent e) {
+		}
+
+	public static void main(String args[]) {
+		long period = (long) 1000.0 / DEFAULT_FPS;
+		// System.out.println("fps: " + DEFAULT_FPS + "; period: " + period +
+		// " ms");
+		new FiechLand(period * 1000000L); // ms --> nanosecs
 	}
 }
