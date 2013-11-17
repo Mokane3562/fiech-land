0f929aeec735776bdaa2e15a494fda87792949aa
RyanTM899213 <rtm773@mun.ca>
2013-11-17 19:23:48 -0330

    Beefed Government class

--- src/ca/proj/game/entities/Government.java
+++ src/ca/proj/game/entities/Government.java
@@ -1,5 +1,34 @@
 package ca.proj.game.entities;
 
-public class Government extends Player {
+import ca.proj.game.level.Level;
+
+public class Government {
+
+	public enum Gov_Type {
+		DEMOCRACY, DICTATORSHIP
+	}
+
+	Gov_Type gov;
+	private int maxSupport = 0;
+	Player leader = null;
+	Level level;
+	
+	public Government(Gov_Type gov, Level level) {
+		this.gov = gov;
+		this.level = level;
+		chooseLeader(level);
+	}
+
+	public void chooseLeader(Level level) {
+		for (Entity e : level.entities) {
+			if (e instanceof Player) {
+				if (((Player)e).support > maxSupport){
+					leader = (Player) e;
+					maxSupport = leader.support;
+				}
+			}
+
+		}
+	}
 
 }
